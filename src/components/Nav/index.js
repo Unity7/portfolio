e7d930fe918738bb02cd50b421362a3bed9f0145
Bay Bui <baybui@ucla.edu>
2021-07-02 19:45:35 -0700

    updated home page

--- src/components/Nav/index.js
+++ src/components/Nav/index.js
@@ -20,10 +20,11 @@ function Nav(props) {
   return (
     <header className="flex-row px-1">
       <h2>
-        <a data-testid="link" href="/">
+        <a data-testid="link" href="https://unity7.github.io/portfolio/">
           <span role="img" aria-label="camera">
             {" "}
-          </span>{" "}
+          </span>
+          {""}
           Bay Bui
         </a>
       </h2>
